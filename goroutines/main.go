4c05db59b3efa6ea4b76f6d904200985ffbbbc9a
Walleks Miranda <walleksmiranda@gmail.com>
2025-04-24 12:25:46 -0300

    feat: add channel

--- goroutines/main.go
+++ goroutines/main.go
@@ -8,12 +8,43 @@ import (
 
 func main() {
 	waitGroup()
+
 	fmt.Printf("\n")
+
 	mutex()
+
+	fmt.Printf("\n")
+
+	fmt.Printf("channel\n\n")
+	channel := make(chan int)
+
+	go setList(channel)
+	// ler do channer
+	channelRead := <-channel
+	fmt.Println("channelRead", channelRead)
+
+	// ler do channer pelo range
+	for value := range channel {
+		fmt.Println("range", value)
+	}
+
+}
+
+// Channel
+func setList(channel chan int) {
+
+	for value := range 10 {
+		// escrever no channel
+		channel <- value
+	}
+	// Precisa fechar o canal, pois nao vai ter nenhum outra goroutines atribuindo a esse canal.
+	close(channel)
 }
 
 // Garatir que diferentes goroutines não altere o mesmo dados, usando o lock
 func mutex() {
+	fmt.Printf("mutex\n\n")
+
 	var m sync.Mutex
 
 	i := 0
@@ -26,11 +57,12 @@ func mutex() {
 			m.Unlock()
 		}()
 	}
-	time.Sleep(time.Second * 2)
+	time.Sleep(time.Second * 1)
 	fmt.Println(i)
 }
 
 func waitGroup() {
+	fmt.Printf("waitGroup\n\n")
 	var wg sync.WaitGroup
 	// Quantidade de processo que ele vai esperar, e isso vai diminuindo conforme o numero de Done for sendo chamado
 	wg.Add(3)
