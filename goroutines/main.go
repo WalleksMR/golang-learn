5f4cc0f17483c78122811f6a5a67204ac57ea2b4
Walleks Miranda <walleksmiranda@gmail.com>
2025-04-24 12:07:30 -0300

    feat: add mutex

--- goroutines/main.go
+++ goroutines/main.go
@@ -7,6 +7,30 @@ import (
 )
 
 func main() {
+	waitGroup()
+	fmt.Printf("\n")
+	mutex()
+}
+
+// Garatir que diferentes goroutines não altere o mesmo dados, usando o lock
+func mutex() {
+	var m sync.Mutex
+
+	i := 0
+
+	for range 1_000 {
+		go func() {
+			m.Lock()
+			// Somente essa goroutine esta alteradando a variavel i
+			i++
+			m.Unlock()
+		}()
+	}
+	time.Sleep(time.Second * 2)
+	fmt.Println(i)
+}
+
+func waitGroup() {
 	var wg sync.WaitGroup
 	// Quantidade de processo que ele vai esperar, e isso vai diminuindo conforme o numero de Done for sendo chamado
 	wg.Add(3)
